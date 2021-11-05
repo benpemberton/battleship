b84719e7eff86bed5b186385edbfef465b33a95f
Ben Pemberton <benedictpemberton@gmail.com>
2021-11-05 17:43:09 +0000

    started testing ship function

---
+++ src/scripts/ship.js
@@ -0,0 +1,26 @@
+function ship(coords) {
+  return {
+    coords: coords,
+    length: coords.length,
+    hits: [],
+    status: "unsunk",
+
+    hit(number) {
+      if (this.coords.includes(number) && !this.hits.includes(number)) {
+        this.hits.push(number);
+      }
+    },
+
+    isSunk() {
+      const sunk = this.hits.every((item) => {
+        return this.coords.includes(item);
+      });
+
+      if (sunk === true && this.hits.length === this.coords.length) {
+        this.status = "sunk";
+      }
+    },
+  };
+}
+
+module.exports = ship;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/scripts/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Battleship",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        sideEffects: true,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.jpg/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
